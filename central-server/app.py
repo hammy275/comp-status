11b94c95060b14de5850c95abe6058e05a453c82
hammy3502 <hammy275@gmail.com>
2020-01-10 14:42:08 -0500

    Fix pcname, server settings

--- central-server/app.py
+++ central-server/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import time
 
 app = Flask(__name__)
 
@@ -11,7 +12,11 @@ def no_page(e):
 
 @app.route("/give_data", methods=["GET", "POST"])
 def give_data():
-    return jsonify(db)
+    return jsonify({"error": 200, "message": "Data successfully received!", "data" : db})
+
+@app.route("/ping", methods=["GET", "POST"])
+def ping():
+    return {"error": 200, "message": "Pong!"}
 
 
 @app.route("/take_data", methods=["POST"])
@@ -19,5 +24,6 @@ def take_data():
     data = request.form.to_dict()
     pc_name = data["pc-name"]
     data.pop("pc-name")
+    data["time"] = int(time.time())
     db[pc_name] = data
     return jsonify({"error": 200, "message": "Data successfully processed!"})
--- server/main.py
+++ server/main.py
@@ -1,8 +1,34 @@
 import requests
 from time import sleep
+import json
 
 import socket
 import psutil
+
+settings = {}
+
+def write_db():
+    with open("settings.json", "w") as dbf:
+        json.dump(settings, dbf)
+
+def startup():
+    global settings
+    try:
+        with open("settings.json") as f:
+            settings = json.load(f)
+    except (json.decoder.JSONDecodeError, FileNotFoundError):
+        while True:
+            ip = input("Enter IP address of central server (including port)! ")
+            try:
+                r = requests.post("http://" + ip + "/ping")
+                if json.loads(r.text)["message"] == "Pong!":
+                    settings["ip"] = ip
+                    write_db()
+                    break
+                else:
+                    print("Invalid IP!")
+            except requests.exceptions.ConnectionError:
+                print("Connection error/invalid IP address!")
 
 
 def main_loop():
@@ -18,17 +44,16 @@ def main_loop():
 
         try:
             r = requests.post("http://localhost:5000/take_data", {
-                "pc-name": "BRADLEY2",
+                "pc-name": pc_name,
                 "current_memory": current_memory, "used_memory": used_memory,
                 "cpu_usage": cpu_usage, "current_turbo": current_turbo, "max_turbo": max_turbo,
 
             })
         except requests.exceptions.ConnectionError:
-            pass
+            print("Failed to send request!")
         sleep(1)
 
 
-
-
 if __name__ == "__main__":
+    startup()
     main_loop()
